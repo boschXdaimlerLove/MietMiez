2e7569a9044e7cfd70ee5406a9b1263dbab5f5cc
brvn0 <hallo@planet-bruno.de>
2025-06-25 13:04:36 +0200

    feat(api): change advertisement endpoint

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -36,6 +36,7 @@ func setupV1Routes(app *fiber.App) {
 	// --- advertisement paths ---
 	v1.Post("/advertisement", controllers.CreateAdvertisement)
 	v1.Get("/advertisement/:id", controllers.AdvertisementInformation)
+	v1.Patch("/advertisement/:id", controllers.UpdateAdvertisement)
 
 	// --- category paths ---
 	v1.Get("/categories", controllers.CategoryList)
