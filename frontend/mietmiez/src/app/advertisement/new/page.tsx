12884e9720b6a1cc00733d58190c5f792919882a
NSZock <nilsnils1212@gmail.com>
2025-07-03 12:14:38 +0200

    Zwischensafe

--- frontend/mietmiez/src/app/advertisement/new/page.tsx
+++ frontend/mietmiez/src/app/advertisement/new/page.tsx
@@ -1,7 +1,86 @@
-export default function NewAdvertisementPage() {
+import Button from "@/app/components/button";
+import { useState } from "react";
+
+const NewAdvertisementPage = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [animal, setAnimal] = useState("");
+    const [error, setError] = useState("");
+
+     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+         e.preventDefault();
+         setError("");
+         ClientUserCommunication.login(email, password).then((success) => {
+           if (!success) {
+             setError("Login fehlgeschlagen. Bitte überprüfe deine Anmeldedaten.");
+           } else {
+             // router.push("/home");
+           }
+         });
+       }
+
     return (
         <main>
-            <p>Create new advertisement here</p>
+            {isLoggedIn ?
+
+            <div>
+                {error && (
+                    <p className="text-red-500 mb-3 text-sm text-center">{error}</p>
+                )}
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Name des Tieres"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                        />
+
+                    <input
+                        type="text"
+                        placeholder="Gib an was man über dein Tier wissen sollte..."
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
+                        />
+
+                    <input
+                        type="text"
+                        placeholder="Art des Tieres"
+                        value={animal}
+                        onChange={(e) => setAnimal(e.target.value)}
+                        required
+                        />
+
+                    <input
+                        type="submit"
+                        placeholder="Veröffentlichen"
+                    ></input>
+                </form>      
+            </div>
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            : <p>Bitte einloggen.</p>}
         </main>
     );
 }
+
+export default NewAdvertisementPage;
---
+++ frontend/mietmiez/src/app/objects/Pet.ts
@@ -0,0 +1,9 @@
+export default class Pet {
+    name: string;
+    species: string;
+
+    constructor(name: string, species: string) {
+        this.name = name;
+        this.species = species;
+    }
+}
