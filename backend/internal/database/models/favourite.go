d56d10bc23e19390dc874b0e7e0cf49dc2e29a21
Gregor <contact@gregors-computer-lab.com>
2025-07-09 13:53:38 +0200

    feat(backend): add POST /favourite endpoint

--- backend/internal/database/models/favourite.go
+++ backend/internal/database/models/favourite.go
@@ -2,10 +2,12 @@ package models
 
 import "gorm.io/gorm"
 
+// Favourite is used to store user specified favourites
+// warning for older db: unique user id with ad id was added later, possible PostgreSQL panic
 type Favourite struct {
 	gorm.Model
-	UserID          uint
-	User            User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	AdvertisementID uint
+	UserID          uint          `gorm:"index:idx_user_advertisement,unique"`
+	User            User          `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	AdvertisementID uint          `gorm:"index:idx_user_advertisement,unique"`
 	Advertisement   Advertisement `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
